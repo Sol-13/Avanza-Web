99de149af8fb2f77e7b5bfd1037e435fc40d1e86
Sol <solealvar13@gmail.com>
2021-12-04 10:48:00 -0300

    datos de abono

--- enviar.php
+++ enviar.php
@@ -3,6 +3,10 @@ $name = $_POST['name'];
 $mail = $_POST['mail'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$dni=$_POST['dni'];
+$edad=$_POST['edad'];
+$fecha=$_POST['fecha'];
+$team=$_POST['team'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -12,10 +16,11 @@ $header .= "Content-Type: text/plain";
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message.="DNI: " . $dni . "\r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'avanza@live.com.ar'; /*pedir mail de avanza*/
+$para = 'solealvar13@gmail.com'; /*pedir mail de avanza*/
 $asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
 
 mail($para, $asunto, utf8_decode($message), $header);
