dae40938d3f8e10560f3179b26a384f110316dbc
unknown <solealvar13@gmail.com>
2021-12-07 20:55:05 -0300

    inscripcionyenvio

--- enviar.php
+++ enviar.php
@@ -16,6 +16,7 @@ $header .= "Content-Type: text/plain";
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Edad: " .$edad. " \r\n";
 $message.="DNI: " . $dni . "\r\n";
  
 $message.= "Distancia:" . $_POST['example'] . " \r\n";
@@ -24,7 +25,7 @@ $message.="Posta:" . $_POST['posta'] . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'avanza@live.com.ar'; /*pedir mail de avanza*/
+$para = 'solealvar13@gmail.com'; /*pedir mail de avanza*/
 $asunto = "Mensaje de: " . $name . " \r\n" ;
 
 mail($para, $asunto, utf8_decode($message), $header);
